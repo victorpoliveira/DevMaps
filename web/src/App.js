cbf9825e4e99c95e56049dbd0c0221a0ea62a8b4
Apokrypho <apokrypho@github.com>
2020-01-21 17:58:09 -0300

    Finalizando a telinha

--- web/src/App.js
+++ web/src/App.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
 import './global.css';
 import './App.css';
 import './Sidebar.css';
+import './Main.css';
 
 function App() {
+  const [github_username, setGithubUsername] = useState('');
+  const [techs, setTechs] = useState('');
+  const [latitude, setLatitude] = useState('');
+  const [longitude, setLongitude] = useState('');
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
+        setLatitude(latitude);
+        setLongitude(longitude);
+      },
+      (err) => {
+        console.log(err);
+      },
+      {
+        timeout: 30000,
+      }
+    )
+  }, []);
+
   return (
     <div id="app">
       <aside>
@@ -11,23 +34,49 @@ function App() {
         <form>
           <div className="input-block">
             <label htmlFor="github_username">Usuário do Github</label>
-            <input name="github_username" id="github_username" required />
+            <input
+              name="github_username"
+              id="github_username"
+              required
+              value={github_username}
+              onChange={e => setGithubUsername(e.target.value)}
+            />
           </div>
 
           <div className="input-block">
             <label htmlFor="techs">Tecnologias</label>
-            <input name="techs" id="techs" required />
+            <input
+              name="techs"
+              id="techs"
+              required
+              value={techs}
+              onChange={e => setTechs(e.target.value)}
+            />
           </div>
 
           <div className="input-group">
             <div className="input-block">
               <label htmlFor="latitude">Latitude</label>
-              <input name="latitude" id="latitude" required />
+              <input
+                name="latitude"
+                id="latitude"
+                required
+                type="number"
+                value={latitude}
+                onChange={e => setLatitude(e.target.value)}
+              />
             </div>
 
             <div className="input-block">
               <label htmlFor="longitude">Longitude</label>
-              <input name="longitude" id="longitude" required />
+              <input
+                name="longitude"
+                id="longitude"
+                required
+                type="number"
+                value={longitude}
+                onChange={e => setLongitude(e.target.value)}
+              />
             </div>
           </div>
 
@@ -36,7 +85,52 @@ function App() {
       </aside>
       
       <main>
-
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars0.githubusercontent.com/u/57244916?s=460&v=4" alt="Apokrypho"/>
+              <div className="user-info">
+              <strong>Apokrypho</strong>
+              <span>ReactJS, React Native, NodeJS</span>
+            </div>
+            </header>
+            <p>Amante da tecnologia, Desenvolvedor, Estudante de segurança.</p>
+            <a href="https://github.com/Apokrypho">Acessar perfil no Github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars0.githubusercontent.com/u/57244916?s=460&v=4" alt="Apokrypho"/>
+              <div className="user-info">
+              <strong>Apokrypho</strong>
+              <span>ReactJS, React Native, NodeJS</span>
+            </div>
+            </header>
+            <p>Amante da tecnologia, Desenvolvedor, Estudante de segurança.</p>
+            <a href="https://github.com/Apokrypho">Acessar perfil no Github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars0.githubusercontent.com/u/57244916?s=460&v=4" alt="Apokrypho"/>
+              <div className="user-info">
+              <strong>Apokrypho</strong>
+              <span>ReactJS, React Native, NodeJS</span>
+            </div>
+            </header>
+            <p>Amante da tecnologia, Desenvolvedor, Estudante de segurança.</p>
+            <a href="https://github.com/Apokrypho">Acessar perfil no Github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars0.githubusercontent.com/u/57244916?s=460&v=4" alt="Apokrypho"/>
+              <div className="user-info">
+              <strong>Apokrypho</strong>
+              <span>ReactJS, React Native, NodeJS</span>
+            </div>
+            </header>
+            <p>Amante da tecnologia, Desenvolvedor, Estudante de segurança.</p>
+            <a href="https://github.com/Apokrypho">Acessar perfil no Github</a>
+          </li>
+        </ul>
       </main>
     </div>
   );
